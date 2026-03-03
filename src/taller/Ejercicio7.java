f9cb9f844e4bdbd7a6d29675aea1cd684dd6e340
josue <josue@josuedebian.josuedavid>
2026-03-02 23:25:11 -0500

    ejercicio7

--- src/taller/Ejercicio7.java
+++ src/taller/Ejercicio7.java
@@ -1,4 +1,21 @@
 package taller;
+import java.util.Scanner;
 
 public class Ejercicio7 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void intercambiar() {
+        System.out.println("Ingrese primer número:");
+        int a = sc.nextInt();
+        System.out.println("Ingrese segundo número:");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
 }
