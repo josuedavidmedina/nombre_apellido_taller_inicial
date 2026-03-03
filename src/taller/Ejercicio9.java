0315c6aae0c24779b3aef5a6c53c15316942c316
josue <josue@josuedebian.josuedavid>
2026-03-02 23:27:36 -0500

    ejercicio9

--- src/taller/Ejercicio9.java
+++ src/taller/Ejercicio9.java
@@ -1,4 +1,21 @@
 package taller;
+import java.util.Scanner;
 
 public class Ejercicio9 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void clasificarEdad() {
+        System.out.println("Ingrese edad:");
+        int edad = sc.nextInt();
+
+        if (edad <= 3)
+            System.out.println("Bebé");
+        else if (edad <= 8)
+            System.out.println("Niño");
+        else if (edad <= 17)
+            System.out.println("Adolescente");
+        else
+            System.out.println("Adulto");
+    }
 }
