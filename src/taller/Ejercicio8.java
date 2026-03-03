53257c3ef03a7b32f1f81f1a54fc3bb094dc12af
josue <josue@josuedebian.josuedavid>
2026-03-02 23:26:12 -0500

    ejercicio8

--- src/taller/Ejercicio8.java
+++ src/taller/Ejercicio8.java
@@ -1,4 +1,11 @@
 package taller;
+import java.util.Random;
 
 public class Ejercicio8 {
+
+    public static void numeroAleatorio() {
+        Random r = new Random();
+        int num = r.nextInt(21) + 50;
+        System.out.println("Número entre 50 y 70: " + num);
+    }
 }
