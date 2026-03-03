426f1ec5015085fef43f76a13134530b4f7cb787
josue <josue@josuedebian.josuedavid>
2026-03-02 23:18:34 -0500

    ejercicio5

--- src/taller/Ejercicio4.java
+++ src/taller/Ejercicio4.java
@@ -1,4 +1,21 @@
 package taller;
+import java.util.Scanner;
 
 public class Ejercicio4 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void parPositivoNegativo() {
+        System.out.println("Ingrese un número entero:");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0 && num > 0)
+            System.out.println("Es par positivo");
+        else if (num % 2 == 0)
+            System.out.println("es par negativo");
+        else if (num > 0)
+            System.out.println("impar positivo");
+        else
+            System.out.println("Impar negativo");
+    }
 }
--- src/taller/Ejercicio5.java
+++ src/taller/Ejercicio5.java
@@ -1,4 +1,24 @@
 package taller;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
 
 public class Ejercicio5 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void calcularEdad() {
+        System.out.println("Ingrese año de nacimiento:");
+        int año = sc.nextInt();
+        System.out.println("Ingrese mes:");
+        int mes = sc.nextInt();
+        System.out.println("Ingrese día:");
+        int dia = sc.nextInt();
+
+        LocalDate nacimiento = LocalDate.of(año, mes, dia);
+        LocalDate hoy = LocalDate.now();
+
+        Period edad = Period.between(nacimiento, hoy);
+        System.out.println("Tiene " + edad.getYears() + " años");
+    }
 }
