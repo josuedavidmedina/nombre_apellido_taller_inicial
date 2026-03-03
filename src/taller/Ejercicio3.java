ab45a8324bc5865835469706fe48983f2a7e0af3
josue <josue@josuedebian.josuedavid>
2026-03-02 23:15:42 -0500

    ejercicio4

--- src/taller/Ejercicio3.java
+++ src/taller/Ejercicio3.java
@@ -1,4 +1,17 @@
 package taller;
+import java.util.Scanner;
 
 public class Ejercicio3 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void parImpar() {
+        System.out.println("Ingrese un número entero:");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0)
+            System.out.println("Es par");
+        else
+            System.out.println("Es impar");
+    }
 }
